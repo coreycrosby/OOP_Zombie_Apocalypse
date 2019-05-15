2aa7cc3f9d9476007ed70afd43d8e6220b5b5e0e
Corey Crosby <ccc_13@live.ca>
2019-05-14 23:15:32 -0400

    completed exercise

---
+++ zombie.py
@@ -0,0 +1,117 @@
+import random
+
+class Zombie:
+
+  max_speed = 5
+  horde = []
+  plague_level = 10
+  default_speed = 1
+  max_strength = 8
+  default_strength = 3
+
+  def __init__(self, speed, strength):
+    """Initializes zombie's speed
+    """
+    if speed > Zombie.max_speed:
+      self.speed = Zombie.default_speed
+    else:
+      self.speed = speed
+
+    """Initializes zombie's strength
+    """
+    if strength > Zombie.max_strength:
+      self.strength = Zombie.default_strength
+    else:
+      self.strength = strength
+
+  def __str__(self):
+      return f"Strength:{self.strength} Speed:{self.speed}"
+
+  @classmethod
+  def spawn(cls):
+    """Spawns a random number of new zombies, based on the plague level,
+    adding each one to the horde.  Each zombie gets a random speed.
+    """
+    new_zombies = random.randint(1, Zombie.plague_level)
+    count = 0
+
+    while count < new_zombies:
+      speed = random.randint(1, Zombie.max_speed)
+      strength = random.randint(1, Zombie.max_strength)
+      Zombie.horde.append(Zombie(strength, speed))
+      count += 1
+
+  @classmethod
+  def new_day(cls):
+    """Represents the events of yet another day of the zombie apocalypse.
+    Every day some zombies die off (phew!), some new ones show up,
+    and sometimes the zombie plague level increases.
+    """
+    Zombie.spawn()
+    Zombie.some_die_off()
+    Zombie.increase_plague_level()
+
+  @classmethod
+  def some_die_off(cls):
+    """Removes a random number (between 0 and 10) of zombies from the horde.
+    """
+    how_many_die = random.randint(0, 10)
+    counter = 0
+    while counter < how_many_die and len(Zombie.horde) > 0:
+      random_zombie = random.randint(0,len(Zombie.horde) - 1)
+      Zombie.horde.pop(random_zombie)
+      counter += 1
+
+  @classmethod
+  def increase_plague_level(cls):
+    day_plague = random.randint(0, 2)
+    Zombie.plague_level += day_plague
+
+  def encounter(self):
+    """This instance method represents you coming across a zombie! This can end in two possible outcomes:
+    1. You outrun the zombie and escape unscathed!
+    2. You don't outrun the zombie. It eats your brains and you die. :(
+    3. Fighting the zombie.
+    Returns a summary of what happened.
+    """
+    outrun = self.chase()
+    fight = self.fight()
+
+    if outrun:
+      return 'You escaped!'
+
+    elif outrun:
+        if fight:  
+            Zombie.horde.append(self)
+            return 'You are now a zombie. Raawwwrghh'
+
+    else:
+      return 'You died.'
+
+  def chase(self):
+    """Represents you trying to outrun this particular zombie.
+    Uses `Zombie.max_speed` to generate a random number that represents how fast you manage to run.
+    """
+    your_speed = random.randint(1, Zombie.max_speed)
+    return your_speed > self.speed
+
+  def fight(self):
+    """represents what happens when you try to fight a zombie."""
+    your_strength = random.randint(1, Zombie.max_strength)
+    return your_strength > self.strength
+
+print(Zombie.horde) # []
+Zombie.new_day()
+print(Zombie.horde) # [<__main__.Zombie object at 0x7f6f594f0d30>, <__main__.Zombie object at 0x7f6f594f0b70>, <__main__.Zombie object at 0x7f6f594f0d68>]
+zombie1 = Zombie.horde[0]
+print(zombie1) # Speed: 1 -- Strength: 7
+zombie2 = Zombie.horde[1]
+print(zombie2) # Speed: 2 -- Strength: 7
+print(zombie1.encounter()) # You escaped!
+print(zombie2.encounter()) # You fought the zombie and caught the plague.  You are now a zombie too.  Raaaawrgh
+Zombie.new_day()
+print(Zombie.horde) # [<__main__.Zombie object at 0x7f6f594f0d30>, <__main__.Zombie object at 0x7f6f594efef0>, <__main__.Zombie object at 0x7f6f594f0c50>, <__main__.Zombie object at 0x7f6f594f0cc0>]
+zombie1 = Zombie.horde[0]
+zombie2 = Zombie.horde[1]
+print(zombie1.encounter()) # You died!
+print(zombie2.encounter()) # You escaped!
